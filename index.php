8e188d88a93ea755761109064ed6e05800436b2a
root <root@ip-172-31-40-139.us-east-2.compute.internal>
2020-06-05 04:54:29 +0000

    second Commit

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 		<p><b>An Example of PHP in Action</b></p>
 		<?php echo "The Current Date and Time is: <br />"; 
 		echo date("g:i A l, F j Y.");?> </p>
-	<h2>PHP Information</h2> 
+	<h2>PHP Information VERSION2 </h2> 
 		<p> <?php phpinfo(); ?> </p> 
 	</body> 
 </html>
