dd7600e65fdce0b31648cb3c0878de0493b9aa00
root <root@ip-172-31-40-139.us-east-2.compute.internal>
2020-06-05 04:46:09 +0000

    first Commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<html> 
+	<head> 
+	<title>PHP Test</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"/>
+	</head>
+
+<body> 
+	<h1>PHP Test</h1>
+		<p><b>An Example of PHP in Action</b></p>
+		<?php echo "The Current Date and Time is: <br />"; 
+		echo date("g:i A l, F j Y.");?> </p>
+	<h2>PHP Information</h2> 
+		<p> <?php phpinfo(); ?> </p> 
+	</body> 
+</html>
