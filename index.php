e9372ad8323cd2a99bb28708a6a6f072dac546ea
mdhack0316 <39210514+mdhack0316@users.noreply.github.com>
2021-05-07 19:22:37 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 <body> 
 	<h1>PHP 1663Test</h1>
-		<p><b>An Example for  Mayank modi ` PHP in Action</b></p>
+		<p><b>An Example for  Mayank modi Openshift Traning ` PHP in Action</b></p>
 		<?php echo "The Current Date and yes las  Time is: <br />";
 		echo date("g:i A l, F j Y.");?> </p>
 	<h2>PHP Information VERSION2 </h2> 
