30be2bcd9f491f243bc3d371cad05670c63789b8
mdhack0316 <39210514+mdhack0316@users.noreply.github.com>
2021-06-12 12:46:55 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 <body> 
 	<h1>PHP 1663Test</h1>
-		<p><b>  Mayank modi ` PHP in Action Hi new code  EX180 EX280</b></p>
+		<p><b>  Mayank modi ` PHP in Action Hi new code  EX180 EX280   ADDINGTIONAL  CONTENT</b></p>
 		<?php echo "The Current Date and yes las  Time is: <br />";
 		echo date("g:i A l, F j Y.");?> </p>
 	<h2>PHP Information VERSION2 </h2> 
