0a6b70d7b208f42a5e1e392cf19f79eb56a823b1
mdhack0316 <39210514+mdhack0316@users.noreply.github.com>
2021-12-10 15:23:29 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,10 +6,10 @@
 
 <body> 
 	<h1>PHP 2021  Test</h1>
-		<p><b>  Mayank modi Version 12</b></p>
+		<p><b>  Mayank modi Version 1</b></p>
 		<?php echo "The Current Date and yes las  Time is: <br />";
 		echo date("g:i A l, F j Y.");?> </p>
-	<h2>PHP Information VERSION2 </h2> 
+	<h2>PHP Information VERSION1 </h2> 
 		<p> <?php phpinfo(); ?> </p> 
 	</body> 
 </html>
