5a337545ef468a52a4d40d1d95f5d2f33ff9064b
mdhack0316 <39210514+mdhack0316@users.noreply.github.com>
2022-01-31 14:52:53 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 <body> 
 	<h1>PHP 2022  Test</h1>
-		<p><b>  Mayank modi CITI BANK  31st jan 2022 </b></p>
+		<p><b> CITIBANK Session </b></p>
 		<?php echo "The Current Date and yes las  Time is: <br />";
 		echo date("g:i A l, F j Y.");?> </p>
 	<h2>PHP Information VERSION 2 </h2> 
