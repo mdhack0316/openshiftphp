cf12c315f0658a29f6163cd7a59d14f865a511df
mdhack0316 <39210514+mdhack0316@users.noreply.github.com>
2022-02-08 20:15:48 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 <body> 
 	<h1>PHP 2022  Test</h1>
-		<p><b> TATA TECHNOLOGIES  Session </b></p>
+		<p><b> TATA TECHNOLOGIES  Session  New Changes  </b></p>
 		<?php echo "The Current Date and yes las  Time is: <br />";
 		echo date("g:i A l, F j Y.");?> </p>
 	<h2>PHP Information VERSION 2 </h2> 
