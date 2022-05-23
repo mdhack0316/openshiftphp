1eb6fbdb2dd8ce28cc7decd6bbe90381a1ed7afa
mdhack0316 <39210514+mdhack0316@users.noreply.github.com>
2022-05-23 14:31:19 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,10 +6,10 @@
 
 <body> 
 	<h1>PHP 2022  Test</h1>
-		<p><b>CITIBANK Session </b></p>
+		<p><b>AMDOCS Session </b></p>
 		<?php echo "The Current Date and yes las  Time is: <br />";
 		echo date("g:i A l, F j Y.");?> </p>
-	<h2>PHP Information VERSION 2 </h2> 
+	<h2>PHP Information VERSION 3 </h2> 
 		<p> <?php phpinfo(); ?> </p> 
 	</body> 
 </html>
