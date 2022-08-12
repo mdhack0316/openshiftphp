b391a6d90ecdedc6b6f1900b7fa9c2f5b720a41f
mdhack0316 <39210514+mdhack0316@users.noreply.github.com>
2022-08-12 17:15:43 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 <body> 
 	<h1>PHP 2022  Test</h1>
-		<p><b>Collins Session Day 5  AWS CICD </b></p>
+		<p><b>Collins Session Day 5  </b></p>
 		<?php echo "The Current Date and yes las  Time is: <br />";
 		echo date("g:i A l, F j Y.");?> </p>
 	<h2>PHP Information </h2> 
