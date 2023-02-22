acf2e966c7e8e78e78700bd0ee9b0bc2ecceef35
mdhack0316 <39210514+mdhack0316@users.noreply.github.com>
2023-02-22 21:04:55 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 <body> 
 	<h1>Mayank MOdis Application </h1>
-		<p><b>DanskeIT Training Day4 </b></p>
+		<p><b>Openshift Batch</b></p>
 		<?php echo "The Current Date and yes las  Time is: <br />";
 		echo date("g:i A l, F j Y.");?> </p>
 	<h2>PHP Information </h2> 
