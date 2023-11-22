8202b874fd24b2323999f4dd8b6ebaba39ea199f
mdhack0316 <39210514+mdhack0316@users.noreply.github.com>
2023-11-22 10:49:46 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 <body> 
 	<h1>Mayank MOdi</h1>
-		<p><b>Openshift Batch Verizon </b></p>
+		<p><b>Openshift Batch Verizon Version 1 </b></p>
 		<?php echo "The Current Date and yes las  Time is: <br />";
 		echo date("g:i A l, F j Y.");?> </p>
 	<h2>PHP Information </h2> 
